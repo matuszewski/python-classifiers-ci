095b463ba2abbdb56de33eb41475e8331932afc2
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2022-01-14 02:31:08 +0100

    removed triggers

--- sample-pipeline.groovy
+++ sample-pipeline.groovy
@@ -1,26 +1,7 @@
 pipeline {
     agent { label 'docker-slave' }
     
-    triggers {
-        GenericTrigger(
-         genericVariables: [
-          [key: 'ref', value: '$.ref']
-         ],
-
-         causeString: 'Triggered on $ref',
-
-         token: '323fbap144',
-         //tokenCredentialId: 'matuszewski-gh-pat',
-
-         printContributedVariables: true,
-         printPostContent: true,
-
-         silentResponse: false,
-
-         regexpFilterText: '$ref',
-         regexpFilterExpression: 'refs/heads/main'
-        )
-    }
+   
     
     stages {
         
