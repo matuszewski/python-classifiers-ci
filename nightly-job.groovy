2bc3c7d7126f1885f6d65b64e8132ec2e0578b45
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2022-01-18 14:58:07 +0100

    Update nightly-job.groovy

--- nightly-job.groovy
+++ nightly-job.groovy
@@ -1,13 +1,60 @@
+def updatePRStatus(String status) { // uses GitHub Integration Plugin https://www.jenkins.io/doc/pipeline/steps/github-pullrequest/     (gitHubPRStatus : )
+    switch(status) {
+        case 'running':
+            script {
+                gitHubPRStatus gitHubPRMessage("Jenkins job is running...")
+            } // end of script
+            break
+        
+        case 'success':
+            script {
+                // Add a comment to GitHub PR
+                gitHubPRComment comment: gitHubPRMessage("<h2>Jenkins job ${JOB_NAME} ended with SUCCESS. Build: ${BUILD_URL}")
+
+                // Set status to GitHub PR check
+                githubPRStatusPublisher (
+                    statusMsg: [
+                        content: "jenkins build succeed"
+                        ],
+                    unstableAs: 'FAILURE',
+                    buildMessage: [
+                        failureMsg: [content: 'build failed!'],
+                        successMsg: [content: 'build succeeded'],
+                        ],
+                    statusVerifier: [buildStatus: 'SUCCESS']
+                )
+            } // end of script
+            break;
+    
+        default:
+            break;
+  } // end of switch
+} // end of updatePRStatus()
+
+
 pipeline {
     agent { label 'docker-slave' }
-  
-    triggers {
-        // trigger with github polling on cron: "at 02:00 on every day-of-week from Monday through Friday"
-        pollSCM('0 2 * * 1-5')
-    }
+    
+    // removed trigerring from jenkinsfile
+    
+    
     
     stages {
-
+        
+        stage('Set running status on GitHub') {
+            steps {
+                script {
+                    if(env.GITHUB_PR_COND_REF) {
+                        echo '[!] triggered by polling'
+                        updatePRStatus('running')
+                    }
+                    else {
+                        echo '[!] manually trigered'
+                    }
+                }
+            }
+        }
+        
         stage('Checkout from GitHub') {
             steps {
                 echo '[>] Trying to checkout from git repository...'
@@ -15,16 +62,17 @@ pipeline {
                     credentialsId: 'matuszewski-gh-pat',
                     url: 'https://github.com/matuszewski/python-classifiers.git'
                 sh "python3 classifiers.py"
-                echo 'Build'
+                echo 'Build done!'
             }
         }
         
-        stage('SonarQube Analysis') {
-            def scannerHome = tool 'SONAR_QUBE';
-            withSonarQubeEnv() {
-                sh "${scannerHome}/bin/sonar-scanner"
-            }
-        }
+        // TODO: SonarQube scan
+        //stage('SonarQube Analysis') {
+        //    def scannerHome = tool 'SONAR_QUBE';
+        //    withSonarQubeEnv() {
+        //        sh "${scannerHome}/bin/sonar-scanner"
+        //    }
+        //}
         
     }
 
@@ -36,14 +84,19 @@ pipeline {
             echo "Pipeline job ${env.JOB_NAME} failed, marked as SUCCESS"
 
             // publish report and archive
-            publishHTML([allowMissing: true, alwaysLinkToLastBuild: false, keepAll: false, reportDir: '.', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: 'HTML Report of successful job execution'])
-            archiveArtifacts allowEmptyArchive: true, artifacts: 'index.html', followSymlinks: false
+            //publishHTML([allowMissing: true, alwaysLinkToLastBuild: false, keepAll: false, reportDir: '.', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: 'HTML Report of successful job execution'])
+            //archiveArtifacts allowEmptyArchive: true, artifacts: 'index.html', followSymlinks: false
             
             // clean up the workspace
-            deleteDir()
+            //deleteDir()
+            
+            // set status on gh pr
+            updatePRStatus('success')
         }
         unstable {
             echo "Pipeline job ${env.JOB_NAME} finished, marked as UNSTABLE"
+            
+
         }
         failure {
             echo "Pipeline job ${env.JOB_NAME} failed, marked as FAILURE"
@@ -52,6 +105,9 @@ pipeline {
             //mail to: 'krzysiekmatuszewski@outlook.com',
             // subject: "Pipeline job failure: ${currentBuild.fullDisplayName}",
             // body: "Build URL: ${env.BUILD_URL}"
+            
+            // set status on gh pr
+            updatePRStatus('failure')
         }
         changed {
             echo 'Job changed state'
